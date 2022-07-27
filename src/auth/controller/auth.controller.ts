86ea4786a354145c0d4d3f36ec31b4ed1fc6ce6a
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-07-27 17:20:05 -0400

    [update] Response

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common'
-import { Request } from 'express'
+import { Controller, Post, Req, Res, UseGuards } from '@nestjs/common'
+import { Request, Response } from 'express'
 import { AuthGuard } from '@nestjs/passport'
 
 import { AuthService } from '../services/auth.service'
+import handleError from 'src/res/handleError'
 
 @Controller('api/authentication')
 export class AuthController {
@@ -10,8 +11,13 @@ export class AuthController {
 
     @UseGuards(AuthGuard('local'))
     @Post('/')
-    login(@Req() req: Request) {
-        const user = req.user
-        return this.authService.generateJWT(user)
+    login(@Req() req: Request, @Res() res: Response) {
+        try {
+            const user = req.user
+            const token = this.authService.generateJWT(user)
+            res.json(token)
+        } catch (err) {
+            handleError(err, res)
+        }
     }
 }
