9e715022120b4f32cb99280c2f2347a55c4698a8
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-12-09 22:17:51 -0300

    [update] Allow headers

--- src/main.ts
+++ src/main.ts
@@ -60,6 +60,7 @@ async function bootstrap() {
         origin: [httpClient, httpsClient],
         methods: ['GET', 'PUT', 'POST', 'DELETE'],
         credentials: true,
+        allowedHeaders: '*',
     })
     // Helmet
     app.use(
