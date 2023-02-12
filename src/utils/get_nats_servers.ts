1a9afe12d10b153ff0ce3974ea61034df2239386
Felipe Cárdenas <felipecardenas301@gmail.com>
2023-02-12 01:13:27 -0300

    [update] NATS Host

--- src/utils/get_nats_servers.ts
+++ src/utils/get_nats_servers.ts
@@ -3,11 +3,7 @@ import config from '../config'
 export function getNatsServers() {
     // Config
     const configService = config()
-
     const hosts = configService.nats.split(',')
-    const ports = configService.nats.split(',')
 
-    return hosts.map((host, i) => {
-        return `nats://${host}:${ports[i]}`
-    })
+    return hosts.map((host) => `nats://${host}`)
 }
